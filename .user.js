08f393d3a49d26ed307d0fe9ac0e393a15c8613e
Zeker0 <zorn@hotmail.co.uk>
2018-04-12 23:04:39 +0100

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         AutoTrimps-kfro
 // @version      1.0-kfro
-// @namespace    https://genbtc.github.io/AutoTrimps
-// @updateURL    https://genbtc.github.io/AutoTrimps/.user.js
+// @namespace    https://KFrowde.github.io/AutoTrimps
+// @updateURL    https://KFrowde.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *genbtc.github.io/AutoTrimps*
+// @connect      *KFrowde.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-script';
+script.id = 'AutoTrimps-KFrowde';
 //This can be edited to point to your own Github Repository URL.
 script.src = 'https://kfrowde.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
