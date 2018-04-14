2ab112d3d0d66b9b0ada7ee60acbcaa92d893f52
KFrowde <37512082+KFrowde@users.noreply.github.com>
2018-04-14 19:18:11 +0100

    qwerty

--- modules/fight-info.js
+++ modules/fight-info.js
@@ -95,7 +95,7 @@
 
 			//Tooltip on world cells
 			//var badName = document.getElementById("badGuyName").innerHTML;
-			$cell.title = document.getElementById("mutationEffects[effectName].text").innerHTML;
+			$cell.title = document.getElementById("mutationEffects[effectName].text");
 
 			if(cell.name.toLowerCase().indexOf('skele') > -1)					// Skeletimp cell
 			{
