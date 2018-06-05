df40c8f64d8e39f88ec68e49d02cd2bacc0eafd9
Zeker0 <zorn@hotmail.co.uk>
2018-06-05 10:03:35 +0100

    Update other.js

--- modules/other.js
+++ modules/other.js
@@ -277,6 +277,7 @@ if (game.global.world==getPageSetting('lootdumpz') && !perked && getPageSetting(
 	    if (getPortalUpgradePrice("Looting_II")+game.resources.helium.totalSpentTemp <= game.resources.helium.respecMax) {
 		buyPortalUpgrade('Looting_II');
 		activateClicked();
+		cancelPortal();
 		debug('Bought ' + getPageSetting('lootdumpa') + ' Looting II');
 	     }
 	else {
