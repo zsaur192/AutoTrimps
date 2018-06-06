34084886e18a0affbba9a7adfff4bf160ad7bed6
Zeker0 <zorn@hotmail.co.uk>
2018-06-06 13:57:19 +0100

    Update battlecalc.js

--- modules/battlecalc.js
+++ modules/battlecalc.js
@@ -227,7 +227,7 @@ function getBattleStats(what,form,crit) {
 	//Amal health
 	if (what == "health" && game.jobs.Amalgamator.owned > 0){
 		var amt = game.jobs.Amalgamator.getHealthMult();
-		currentCalc *= (1 + (amt / 100));
+		currentCalc *= amt;
 	}
     if (crit) {
         var critChance = getPlayerCritChance();
@@ -301,7 +301,7 @@ function calcOurDmg(number,maxormin,disableStances,disableFlucts) { //number = b
         number *= game.jobs.Amalgamator.getDamageMult();
     }
     if (game.jobs.Amalgamator.owned > 0){
-        number *= ((game.jobs.Amalgamator.getHealthMult() / 100) + 1);
+        number *= game.jobs.Amalgamator.getHealthMult();
     }
     number *= (1 + (1 - game.empowerments.Ice.getCombatModifier()));
 
