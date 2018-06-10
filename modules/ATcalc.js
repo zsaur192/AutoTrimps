3e2e56bbfe7d702e807959de6ced29ddb81aa489
Zeker0 <zorn@hotmail.co.uk>
2018-06-10 15:54:04 +0100

    Create ATcalc.js

---
+++ modules/ATcalc.js
@@ -0,0 +1,100 @@
+var trimpATK = calculateDamageAT(game.global.soldierCurrentAttack, true, true);
+
+function calculateDamageAT(number, buildString, isTrimp, noCheckAchieve, cell) { //number = base attack
+    var fluctuation = .2; //%fluctuation
+	var maxFluct = -1;
+	var minFluct = -1;
+	if (isTrimp){
+		//Situational Trimp damage increases
+		if (game.jobs.Amalgamator.owned > 0){
+			number *= game.jobs.Amalgamator.getDamageMult();
+		}
+		if (game.challenges.Electricity.stacks > 0) { //Electricity
+			number *= (1 - (game.challenges.Electricity.stacks * 0.1));
+		}
+		if (game.global.antiStacks > 0) {
+			number *= ((game.global.antiStacks * game.portal.Anticipation.level * game.portal.Anticipation.modifier) + 1);
+			updateAntiStacks();
+		}
+		if (!game.global.mapsActive && game.global.mapBonus > 0){
+			number *= ((game.global.mapBonus * .2) + 1);
+		}
+		if (game.global.titimpLeft >= 1 && game.global.mapsActive){
+			number *= 2;
+		}
+		if (game.global.achievementBonus > 0){
+			number *= (1 + (game.global.achievementBonus / 100));
+		}
+		if (game.global.challengeActive == "Discipline"){
+			fluctuation = .995;
+		}
+		else if (game.portal.Range.level > 0){
+			minFluct = fluctuation - (.02 * game.portal.Range.level);
+		}
+		if (game.global.challengeActive == "Decay"){
+			number *= 5;
+			number *= Math.pow(0.995, game.challenges.Decay.stacks);
+		}
+		if (game.global.roboTrimpLevel > 0){
+			number *= ((0.2 * game.global.roboTrimpLevel) + 1);
+		}
+		if (game.global.challengeActive == "Lead" && ((game.global.world % 2) == 1)){
+			number *= 1.5;
+		}
+		if (game.goldenUpgrades.Battle.currentBonus > 0){
+			number *= game.goldenUpgrades.Battle.currentBonus + 1;
+		}
+		if (game.talents.voidPower.purchased && game.global.voidBuff){
+			var vpAmt = (game.talents.voidPower2.purchased) ? ((game.talents.voidPower3.purchased) ? 65 : 35) : 15;
+			number *= ((vpAmt / 100) + 1);
+		}
+		if (game.global.totalSquaredReward > 0){
+			number *= ((game.global.totalSquaredReward / 100) + 1)
+		}
+		if (getEmpowerment() == "Ice"){
+			number *= 1 + (1 - game.empowerments.Ice.getCombatModifier());
+		}
+		if (game.talents.magmamancer.purchased){
+			number *= game.jobs.Magmamancer.getBonusPercent();
+		}
+		if (game.talents.stillRowing2.purchased){
+			number *= ((game.global.spireRows * 0.06) + 1);
+		}
+		if (game.talents.healthStrength.purchased && mutations.Healthy.active()){
+			number *= ((0.15 * mutations.Healthy.cellCount()) + 1);
+		}
+		if (game.global.sugarRush > 0){
+			number *= sugarRush.getAttackStrength();
+		}
+		if (game.global.challengeActive == "Life") {
+			number *= game.challenges.Life.getHealthMult();
+		}
+		if (game.singleRunBonuses.sharpTrimps.owned){
+			number *= 1.5;
+		}
+		if (game.global.challengeActive == "Daily"){
+			if (typeof game.global.dailyChallenge.minDamage !== 'undefined'){
+				if (minFluct == -1) minFluct = fluctuation;
+				minFluct += dailyModifiers.minDamage.getMult(game.global.dailyChallenge.minDamage.strength);
+			}
+			if (typeof game.global.dailyChallenge.maxDamage !== 'undefined'){
+				if (maxFluct == -1) maxFluct = fluctuation;
+				maxFluct += dailyModifiers.maxDamage.getMult(game.global.dailyChallenge.maxDamage.strength);
+			}
+			if (typeof game.global.dailyChallenge.weakness !== 'undefined'){
+				number *= dailyModifiers.weakness.getMult(game.global.dailyChallenge.weakness.strength, game.global.dailyChallenge.weakness.stacks);
+			}
+			if (typeof game.global.dailyChallenge.oddTrimpNerf !== 'undefined' && ((game.global.world % 2) == 1)){
+					number *= dailyModifiers.oddTrimpNerf.getMult(game.global.dailyChallenge.oddTrimpNerf.strength);
+			}
+			if (typeof game.global.dailyChallenge.evenTrimpBuff !== 'undefined' && ((game.global.world % 2) == 0)){
+					number *= dailyModifiers.evenTrimpBuff.getMult(game.global.dailyChallenge.evenTrimpBuff.strength);
+			}
+			if (typeof game.global.dailyChallenge.rampage !== 'undefined'){
+				number *= dailyModifiers.rampage.getMult(game.global.dailyChallenge.rampage.strength, game.global.dailyChallenge.rampage.stacks);
+			}
+		}
+		if (Fluffy.isActive()){
+			number *= Fluffy.getDamageModifier();
+		}
+
