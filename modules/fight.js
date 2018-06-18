8762197a2c5389ea6c4cc477f31bcd03def775ee
Zeker0 <zorn@hotmail.co.uk>
2018-06-18 22:25:43 +0100

    Update fight.js

--- modules/fight.js
+++ modules/fight.js
@@ -101,11 +101,10 @@ function betterAutoFight3() {
     if (!game.global.fighting && !game.global.preMapsActive) {
         buyArms();
     }
-    if (((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= 45) {
-          if (game.global.antiStacks < 45 && !game.global.preMapsActive) {
+    if (((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= 45 && game.global.antiStacks < 45 && !game.global.preMapsActive) {
     mapsClicked();
       }
-    }
+    
     if (!game.global.fighting) {
         fightManual();
     }
