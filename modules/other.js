c37e2d3c8d1194b7ef1ba16ed1109ca76a65b220
Zeker0 <zorn@hotmail.co.uk>
2018-06-24 15:49:12 +0100

    Update other.js

--- modules/other.js
+++ modules/other.js
@@ -449,5 +449,6 @@ antistacklimit = 30;
 }
 if (((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && game.global.antiStacks < antistacklimit) {
      forceAbandonTrimps();
+     ('Killing trimps, Getting ' + antistacklimit + ' stacks');
       }
 }
