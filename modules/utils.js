11b99df5643689d3d9696e3df43c9c8e716afe07
Zeker0 <zorn@hotmail.co.uk>
2018-06-29 08:43:21 +0100

    Revert "Update utils.js"
    This reverts commit 56ffecba0704ccc50ce8d8fb7da9060484fe49cd.

--- modules/utils.js
+++ modules/utils.js
@@ -114,6 +114,7 @@ function serializeSettings() {
         case 'boolean':
             return v[k] = el.enabled, v;
         case 'value':
+	case 'multiValue':
         case 'valueNegative':
         case 'multitoggle':
             return v[k] = el.value, v;
