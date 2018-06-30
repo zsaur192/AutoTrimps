1d81a2591840266f4c13ec3005a1f2e1067ba443
Zeker0 <zorn@hotmail.co.uk>
2018-06-30 20:42:15 +0100

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -212,6 +212,7 @@ function mainLoop() {
 //Extra
 
     if (getPageSetting('ExitSpireCell') > 0) exitSpireCell(); //"Exit Spire After Cell" (other.js)
+    if (getPageSetting('dexitspirecell') >= 1) dailyexitSpireCell();
     if (getPageSetting('SpireBreedTimer') > 0) ATspirebreed(); //breedtimer.js
     if (getPageSetting('trimpsnotdie')==true) helptrimpsnotdie(); //other.js
     if (getPageSetting('Praidingzone').length && game.global.challengeActive != "Daily") Praiding(); //Prestige Raiding (other.js)
