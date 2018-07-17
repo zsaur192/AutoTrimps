953aed59190d669d8147fcec4e5fbeb59c1f5681
spikenslab <spikenslab@garyc.co.uk>
2018-07-17 11:58:20 +0100

    Revert "Merge pull request #1 from spikenskab/testconfig"
    This reverts commit 0693e2306b0c4aeacc308ce5d77261cc72f683a2, reversing
    changes made to 70e458c7b351f86ba0b39f132dee040d98358a72.

--- .user.js
+++ .user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
-// @name         AutoTrimps-Zek-test
+// @name         AutoTrimps-Zek
 // @version      1.0-Zek
-// @namespace    https://spikenskab.github.io/AutoTrimps
-// @updateURL    https://spikenskab.github.io/AutoTrimps/.user.js
+// @namespace    https://Zorn192.github.io/AutoTrimps
+// @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *spikenskab.github.io/AutoTrimps*
+// @connect      *Zorn192.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek-test';
+script.id = 'AutoTrimps-Zek';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://spikenskab.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
