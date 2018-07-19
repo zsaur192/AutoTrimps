ad8ea503976c0c9758afbd8c8c86a8a9fee1c0df
Zeker0 <zorn@hotmail.co.uk>
2018-07-19 17:21:33 +0100

    Update magmite.js

--- modules/magmite.js
+++ modules/magmite.js
@@ -157,7 +157,7 @@ function autoGenerator() {
     //if (autoGenerator3);
     if (!autoGenOverrides()) {
       const lateMode = getPageSetting('AutoGen3');
-      if (game.global.generatorMode != lateMode)
+      if (game.global.generatorMode != lateMode && getPageSetting('fuellater') < 1 || game.global.world >= getPageSetting('fuellater'))
         changeGeneratorState(lateMode);
     }
   } else autoGenerator2();
@@ -177,7 +177,7 @@ function autoGenerator2() {
   const mode = getPageSetting('AutoGen2'); // None : Microtick : Cap
   if (!mode) // Default: move on
     return;
-  else if (mode == 3 && game.generatorUpgrades["Overclocker"].upgrades > 0) { // Only trigger overclock if we have Overclocker upgrades.
+  else if (mode == 3 && game.generatorUpgrades["Overclocker"].upgrades > 0 && getPageSetting('fuellater') < 1 || game.global.world >= getPageSetting('fuellater')) { // Only trigger overclock if we have Overclocker upgrades.
     changeGeneratorState(FUEL);
     return;
   }
