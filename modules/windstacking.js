bb5643b7c2cd56fe3cb4021e6ca98cb7f821406c
Zeker0 <zorn@hotmail.co.uk>
2018-08-12 20:44:14 +0100

    Update windstacking.js

--- modules/windstacking.js
+++ modules/windstacking.js
@@ -1,1 +1,19 @@
-//yes
+MODULES["windstacking"] = {};
+
+function windstacking() {
+         var windstackzone = getPageSetting('ultwindstackzone');
+         var HD = HDratioy();
+         var needequipwind = false;
+         var needcoordwind = false;
+         var needmapwind = false;
+         var world = game.global.world
+         if (HD < 0.00025 && world >= (windstackzone - 5) {
+             MODULES["equipment"].enoughDamageCutoff = 90
+             MODULES["maps"].enoughDamageCutoff = 90
+             }
+         if (HD > 0.00025 && world >= (windstackzone - 5) {
+             MODULES["equipment"].enoughDamageCutoff = 4
+             MODULES["maps"].enoughDamageCutoff = 4
+             }
+         if (meh cant be bothered) dontdothis();
+}
