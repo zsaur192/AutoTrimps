53b8b8f2b2918c11c72952b7dda2c6aab480977b
Zeker0 <zorn@hotmail.co.uk>
2018-08-13 14:25:28 +0100

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -233,7 +233,6 @@ function mainLoop() {
     if (getPageSetting('ForceAbandon')==true) trimpcide(); //other.js
     if (getPageSetting('AutoAllocatePerks')==2) lootdump(); //Loot Dumping (other.js)
     if (!game.singleRunBonuses.heliumy.owned && game.global.challengeActive == "Daily" && getPageSetting('buyheliumy') >= 1) heliumydaily();
-    if (getPageSetting('buynojobsc')==true || getPageSetting('buynojobsc')==false) buynojobs();
     if (getPageSetting('fightforever')==true || (getPageSetting('cfightforever')==true && (game.global.challengeActive == 'Toxicity' || game.global.challengeActive == 'Nom')) || (getPageSetting('dfightforever')==true && typeof game.global.dailyChallenge.empower == 'undefined' && typeof game.global.dailyChallenge.bloodthirst == 'undefined' && (typeof game.global.dailyChallenge.bogged !== 'undefined' || typeof game.global.dailyChallenge.plague !== 'undefined' || typeof game.global.dailyChallenge.pressure !== 'undefined'))) fightalways();
     if (getPageSetting('use3daily')==true || getPageSetting('use3daily')==false) usedaily3();
     if (getPageSetting('windcutoff')>=1 && game.global.challengeActive != "Daily") cutoffwind();
