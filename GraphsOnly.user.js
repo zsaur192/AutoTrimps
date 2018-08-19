8c1b4738ef7b183bea67f504381a5f845ba25d51
Zeker0 <zorn@hotmail.co.uk>
2018-08-19 19:13:51 +0100

    Update GraphsOnly.user.js

--- GraphsOnly.user.js
+++ GraphsOnly.user.js
@@ -9,16 +9,6 @@
 // @include      *kongregate.com/games/GreenSatellite/trimps
 // @grant        none
 // ==/UserScript==
-function safeSetItems(name,data) {
-    try {
-        localStorage.setItem(name, data);
-    } catch(e) {
-      if (e.code == 22) {
-        // Storage full, maybe notify user or do some clean-up
-        debug("Error: LocalStorage is full, or error. Attempt to delete some portals from your graph or restart browser.");
-      }
-    }
-}
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Graphs';
 script.src = 'https://Zorn192.github.io/AutoTrimps/GraphsOnly.js';
