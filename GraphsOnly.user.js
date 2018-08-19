9c9bb85796fa49dea4135e651bb9530f799ef26d
Zeker0 <zorn@hotmail.co.uk>
2018-08-19 19:12:24 +0100

    Update GraphsOnly.user.js

--- GraphsOnly.user.js
+++ GraphsOnly.user.js
@@ -21,6 +21,6 @@ function safeSetItems(name,data) {
 }
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Graphs';
-script.src = 'https://Zorn192.github.io/AutoTrimps/Graphs.js';
+script.src = 'https://Zorn192.github.io/AutoTrimps/GraphsOnly.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
