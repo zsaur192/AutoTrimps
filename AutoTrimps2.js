e5c3bdb7fd86c53bad792b7cb40653dd2c1943be
Zeker0 <zorn@hotmail.co.uk>
2018-08-22 18:21:43 +0100

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -61,7 +61,7 @@ function initializeAutoTrimps() {
 }
 
 var changelogList = [];
-changelogList.push({date: "22/08/2018", version: "v2.6.3", description: "Added <b>H:D Map Values</b> can now be controlled. <b>Disable Farming</b> is also now controllable, so <b>make sure to set it!</b> Added <b>Magic Armor</b> option to Dailys/C2s. The Daily tab is getting really fat now, oh well lol. ", isNew: true});
+changelogList.push({date: "22/08/2018", version: "v2.6.3", description: "<b>Disable Farming</b> is now controllable, so <b>make sure to set it!</b> Added <b>Magic Armor</b> option to Dailys/C2s. The Daily tab is getting really fat now, oh well lol. ", isNew: true});
 changelogList.push({date: "21/08/2018", version: "v2.6.2", description: "<b>Amal Boost</b> can now be controlled. <b>Disable Farming</b> now has a more intuitive layout, so make sure to set it. You can also mess with <b>Scryer</b> Min/Max Zones better. Some new WS options to be added later. ", isNew: false});
 changelogList.push({date: "19/08/2018", version: "v2.6.1", description: "<b>Windstacking</b> now has its own tab. Added <b>Ultimate Windstacking.</b> There may be some other tweaks that I\'ve forgot about, but have fun experimenting! ", isNew: false});
 changelogList.push({date: "16/08/2018", version: "v2.5.3", description: "New <b>Magma</b> settings overhaul, it should be easier to understand now. Make sure they\'re set properly. Please let me know if something doesn\'t work with it. There\'s probably more stuff I've added or changed but I forgot lol. ", isNew: false});
