b0f5ba7e80f032931d8614c14a09192e3a47b51c
Zeker0 <zorn@hotmail.co.uk>
2018-08-24 19:10:21 +0100

    Update fight.js

--- modules/fight.js
+++ modules/fight.js
@@ -32,55 +32,51 @@ function betterAutoFight() {
 function betterAutoFight2() {
     var customVars = MODULES["fight"];
     if (game.global.autoBattle && !game.global.pauseFight)
-        pauseFight();   //Disable built-in autofight
+        pauseFight();
     if (game.global.gridArray.length === 0 || game.global.preMapsActive || !game.upgrades.Battle.done || game.global.fighting)
-        return;         //sanity check.
+        return;
     var spireBreed = getPageSetting('SpireBreedTimer');
-      var targetBreed = (game.global.spireActive && spireBreed != -1) ? spireBreed : game.global.GeneticistassistSetting; //use custom breed value for spire
+    var targetBreed = (game.global.spireActive && spireBreed != -1) ? spireBreed : game.global.GeneticistassistSetting; //use custom breed value for spire
     var breeding = (game.resources.trimps.owned - game.resources.trimps.employed);
     var newSquadRdy = game.resources.trimps.realMax() <= game.resources.trimps.owned + 1;
     var adjustedMax = (game.portal.Coordinated.level) ? game.portal.Coordinated.currentSend : trimps.maxSoldiers;
     var potencyMod = getPotencyMod();
     var tps = breeding * potencyMod;
     var addTime = adjustedMax / tps;
-    //if armySend is less than half of what you have breeding, and what you have breeding is more than 10% of your total trimps. (when scientist I is incompleted)
     var lowLevelFight = game.resources.trimps.maxSoldiers < 0.5*breeding && breeding > 0.1*game.resources.trimps.realMax() && game.global.world <= 6 && game.global.sLevel < 1;
-
-    //chooses the correct patience value
     var breedTimerLimit = game.talents.patience.purchased && getPageSetting('UsePatience') ? 46 : 31;
 
-    //Manually fight if:     //game.global.soldierHealth > 0 //just fight if we're alive,or if == 0; we're dead, and also fight :P
-    if (!game.global.fighting) { //  !(game.global.spireActive || (game.global.mapsActive && getCurrentMapObject().location == "Void") || game.global.preMapsActive)
+    
+    if (!game.global.fighting) { 
         if (game.global.SpireActive){
           if((game.global.lastBreedtime/1000)>=targetBreed && (game.global.lastBreedtime/1000)>=breedTimerLimit) {
-            fightManual()
+            fightManual();
           }
         }
-      else if (game.global.soldierHealth > 0)getPageSetting('AutoMaps') == 1
-        fightManual(); //If you are not fighting but have health, fix this situation by clicking fight (fixes some weird Trimps errors)
-       //if max trimps, army size insignificant, or in Watch
+      else if (game.global.soldierHealth > 0 && getPageSetting('AutoMaps') == 1)
+        fightManual();
       if (newSquadRdy || lowLevelFight || game.global.challengeActive == 'Watch') {
         fightManual();
       }
-      //Click Fight if we are dead and already have enough for our breed timer, and fighting would not add a significant amount of time
+      
       else if (getBreedTime() < customVars.breedTimerCutoff1 && (game.global.lastBreedTime/1000) > targetBreed) {
         fightManual();
         if (MODULES["fight"].enableDebug)
         debug("AutoFight: BAF2 #1, breed &lt; " + customVars.breedTimerCutoff1 + " &amp;&amp; HiddenNextGroup &gt; GeneTimer", "other");
       }
-      //AutoFight will now send Trimps to fight if it takes less than 0.5 seconds to create a new group of soldiers, if we havent bred fully yet
+     
       else if (getBreedTime() <= customVars.breedTimerCutoff2) {
         fightManual();
         if (MODULES["fight"].enableDebug)
         debug("AutoFight: BAF2 #2, breed &lt;= " + customVars.breedTimerCutoff2 + " s", "other");
       }
-      //Click fight anyway if we are dead and stuck in a loop due to Dimensional Generator and we can get away with adding time to it.
+     
       else if (getBreedTime(true)+addTime <= targetBreed && breeding>=adjustedMax && !(game.global.mapsActive && getCurrentMapObject().location == "Void")) {
         fightManual();
         if (MODULES["fight"].enableDebug)
         debug("AutoFight: BAF2 #3, RemainingTime + ArmyAdd.Time &lt; GeneTimer", "other");
       }
-      //Clicks fight anyway if we are dead and have >=breedTimerLimit NextGroupTimer and deal with the consequences by firing geneticists afterwards.
+
       else if (game.global.soldierHealth == 0 && (game.global.lastBreedTime/1000)>=breedTimerLimit && targetBreed >= 0 && !game.jobs.Geneticist.locked && game.jobs.Geneticist.owned > 10 ) {
         fightManual();
         if (MODULES["fight"].enableDebug)
