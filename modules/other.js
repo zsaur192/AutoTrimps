8080ae9ba75c7026095ae50693d4d6df7c3e46c9
Zeker0 <zorn@hotmail.co.uk>
2018-08-24 11:54:23 +0100

    Update other.js

--- modules/other.js
+++ modules/other.js
@@ -657,9 +657,9 @@ if (game.portal.Anticipation.level >= 1) {
 	    antistacklimit = 30;
 	if (getPageSetting('fuckanti') > 0 && getEmpowerment() == "Wind" && game.global.world >= getPageSetting('hardcorewind') && (game.global.world < getPageSetting('hardcorewindmax') || getPageSetting('hardcorewindmax') < 1) && HDratioy() < getPageSetting('fuckanti'))
 	    antistacklimit = 0;
-	if ((((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit || antistacklimit == 0 && game.global.antiStacks >= 1) && (game.global.antiStacks < antistacklimit || antistacklimit == 0 && game.global.antiStacks >= 1) && !game.global.spireActive) {
+	if (((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && (game.global.antiStacks < antistacklimit || antistacklimit == 0 && game.global.antiStacks >= 1) && !game.global.spireActive) {
               forceAbandonTrimps();
-		if (getPageSetting('fuckanti') > 0 && game.global.antiStacks != 0)
+		if (getPageSetting('fuckanti') > 0 && game.global.antiStacks != 0 && !game.global.fighting)
 	    	    fightManual();
 	}
 	if (((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && game.global.antiStacks < antistacklimit && game.global.mapsActive) {
