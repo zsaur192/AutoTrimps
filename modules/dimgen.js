74ce705c29f1bfac355e8fdfc834ddda259b6270
Zeker0 <zorn@hotmail.co.uk>
2018-08-26 19:10:39 +0100

    Update dimgen.js

--- modules/dimgen.js
+++ modules/dimgen.js
@@ -1,23 +1,18 @@
 MODULES["dimgen"] = {};
-//This Dimensional Generator has likely been ==DEPRECATED== in favor of Magmite.js which is a complete Magma management system. 
-// If you choose to enable the simpler one, replace 'magmite' with 'dimgen' on line 32 of AutoTrimps2.js - var ATmodules = [...]
 MODULES["dimgen"].FuelOnlyZone = 250;
 MODULES["dimgen"].HybridZone = 250;
 MODULES["dimgen"].MagmiteZone = 400;
 MODULES["dimgen"].FuelOnlyOverclock = false;
 
-//Initialize Global Vars (dont mess with these ones, nothing good can come from it).
 var mapsForFuel = false;
 
 function autodimgen() {
     mapsForFuel = false;
-    if (game.global.word < 230) {
-        return;
-    }
+    if (game.global.word < 230) return;
     var customVars = MODULES["dimgen"];
     var fuelCap = getGeneratorFuelCap();
     var autoCurrentFuel = game.global.magmaFuel;
-    var fuelPerCurrentCell = Math.min(game.generatorUpgrades.Supply.modifier, 0.2 + ((game.global.world - 230) * 0.01))
+    var fuelPerCurrentCell = Math.min(game.generatorUpgrades.Supply.modifier, 0.2 + ((game.global.world - 230) * 0.01));
     var wouldOverFuel = (game.permanentGeneratorUpgrades.Storage.owned) ? (autoCurrentFuel > fuelCap + 2) : (2 * fuelPerCurrentCell + autoCurrentFuel > fuelCap - 0.01);
     if (game.global.world < customVars.FuelOnlyZone) {
         if (game.global.generatorMode != 1) changeGeneratorState(1);
