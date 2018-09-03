dc771cf1e68928e8ed75c824de4228b9f72122a5
Zeker0 <zorn@hotmail.co.uk>
2018-09-03 12:19:54 +0100

    Update fight.js

--- modules/fight.js
+++ modules/fight.js
@@ -1,15 +1,13 @@
 MODULES["fight"] = {};
-//These can be changed (in the console) if you know what you're doing:
 MODULES["fight"].breedTimerCutoff1 = 2;
 MODULES["fight"].breedTimerCutoff2 = 0.5;
-MODULES["fight"].enableDebug = true;    //controls whether betterAutoFight2 is Spammy or not.
+MODULES["fight"].enableDebug = true;
 
-//old: Handles manual fighting automatically, in a different way.
 function betterAutoFight() {
     var customVars = MODULES["fight"];
     if (game.global.autoBattle && !game.global.pauseFight)
         pauseFight();
-    if (game.global.gridArray.length === 0 || game.global.preMapsActive || !game.upgrades.Battle.done) return;  //sanity check. stops error message on z1 right after portal
+    if (game.global.gridArray.length === 0 || game.global.preMapsActive || !game.upgrades.Battle.done) return;
     var targetBreed = getPageSetting('GeneticistTimer');
     var breeding = (game.resources.trimps.owned - game.resources.trimps.employed);
     var newSquadRdy = game.resources.trimps.realMax() <= game.resources.trimps.owned + 1;
@@ -21,7 +19,6 @@ function betterAutoFight() {
     }
 }
 
-//NEW:: 2nd algorithm for Better Auto Fight
 function betterAutoFight2() {
     var customVars = MODULES["fight"];
     if (game.global.autoBattle && !game.global.pauseFight)
@@ -36,8 +33,6 @@ function betterAutoFight2() {
     var tps = breeding * potencyMod;
     var addTime = adjustedMax / tps;
     var lowLevelFight = game.resources.trimps.maxSoldiers < 0.5*breeding && breeding > 0.1*game.resources.trimps.realMax() && game.global.world <= 6 && game.global.sLevel < 1;
-
-    
     if (!game.global.fighting) { 
         if (game.global.SpireActive){
           if((game.global.lastBreedtime/1000)>=targetBreed && (game.global.lastBreedtime/1000)>=breedTimerLimit) {
@@ -52,13 +47,12 @@ function betterAutoFight2() {
     }
 }
 
-//NEW:: 3rd algorithm for Better Auto Fight (Basically brute force fight unless in spire or VM)
 function betterAutoFight3() {
     var customVars = MODULES["fight"];
-    if (game.global.autoBattle && game.global.pauseFight && !game.global.spireActive) //check autofight available, and if it is off
-        pauseFight(); //activates autofight
+    if (game.global.autoBattle && game.global.pauseFight && !game.global.spireActive)
+        pauseFight();
         if (game.global.gridArray.length === 0 || game.global.preMapsActive || !game.upgrades.Battle.done || game.global.fighting || game.global.spireActive)
-            return;         //sanity check.
+            return;
         if (game.global.world == 1 && !game.global.fighting) {
             fightManual();
         }
