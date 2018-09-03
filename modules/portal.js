8f0ffe1d615e579cb743b0b8db59e598060b947b
Zeker0 <zorn@hotmail.co.uk>
2018-09-03 16:26:39 +0100

    Update portal.js

--- modules/portal.js
+++ modules/portal.js
@@ -323,7 +323,7 @@ function c2listcolor(){
 }
 
 function c2listcolor2() {
-["Size", "Watch", "Discipline", "Balance", "Meditate", "Metal", "Lead", "Nom", "Electricity", "Toxicity", "Coordinate", "Trimp", "Obliterated", "Mapology", "Trapper"].forEach(function(what) {
+["Size", "Slow", "Watch", "Discipline", "Balance", "Meditate", "Metal", "Lead", "Nom", "Electricity", "Toxicity", "Coordinate", "Trimp", "Obliterated", "Mapology", "Trapper"].forEach(function(what) {
 if ((game.c2[what]/game.global.highestLevelCleared*100).toFixed(2) >= 95) c2list[what].color = "green";
 if ((game.c2[what]/game.global.highestLevelCleared*100).toFixed(2) < 95 && (game.c2[what]/game.global.highestLevelCleared*100).toFixed(2) >= 85) c2list[what].color = "yellow";
 if ((game.c2[what]/game.global.highestLevelCleared*100).toFixed(2) < 85) c2list[what].color = "red";
