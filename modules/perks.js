9525ec191fcacfb7802746e0c798d9f30dc436f6
Zeker0 <zorn@hotmail.co.uk>
2018-09-05 07:04:01 +0100

    Update perks.js

--- modules/perks.js
+++ modules/perks.js
@@ -671,7 +671,7 @@ AutoPerks.initializePerks = function () {
     var carpentry_II = new AutoPerks.ArithmeticPerk("carpentry_II", 100000, 10000, 0.0025, carpentry);
     var looting_II = new AutoPerks.ArithmeticPerk("looting_II", 100000, 10000, 0.0025, looting);
 
-    AutoPerks.perkHolder = [siphonology, anticipation, meditation, relentlessness, range, agility, bait, trumps, packrat, looting, toughness, power, motivation, pheromones, artisanistry, carpentry, resilience, coordinated, resourceful, overkill, capable, cunning, curious, /*classy,*/ toughness_II, power_II, motivation_II, carpentry_II, looting_II];
+    AutoPerks.perkHolder = [siphonology, anticipation, meditation, relentlessness, range, agility, bait, trumps, packrat, looting, toughness, power, motivation, pheromones, artisanistry, carpentry, resilience, coordinated, resourceful, overkill, capable, cunning, curious, classy, toughness_II, power_II, motivation_II, carpentry_II, looting_II];
     for(var i in AutoPerks.perkHolder) {
         AutoPerks.perkHolder[i].level = 0;
         AutoPerks.perkHolder[i].spent = 0;
