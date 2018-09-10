df85a482d9d4b9d02aee9dc40e1ac08e24d97918
Zeker0 <zorn@hotmail.co.uk>
2018-09-10 10:12:42 +0100

    Update scryer.js

--- modules/scryer.js
+++ modules/scryer.js
@@ -1,9 +1,9 @@
 let wantToScry = false;
 
 function canNatureScry() {
-    const scryInPoisonTrue = getEmpowerment() === "Poison" && (getPageSetting('ScryUseinPoison') < 0) || (game.global.world >= getPageSetting('ScryUseinPoison'));
-    const scryInWindTrue = getEmpowerment() === "Wind" && (getPageSetting('ScryUseinWind') < 0) || (game.global.world >= getPageSetting('ScryUseinWind'));
-    const scryInIceTrue = getEmpowerment() === "Ice" && (getPageSetting('ScryUseinIce') < 0) || (game.global.world >= getPageSetting('ScryUseinIce'));
+    const scryInPoisonTrue = getEmpowerment() === "Poison" && (getPageSetting('ScryUseinPoison') < 0 || game.global.world >= getPageSetting('ScryUseinPoison'));
+    const scryInWindTrue = getEmpowerment() === "Wind" && (getPageSetting('ScryUseinWind') < 0 || game.global.world >= getPageSetting('ScryUseinWind'));
+    const scryInIceTrue = getEmpowerment() === "Ice" && (getPageSetting('ScryUseinIce') < 0 || game.global.world >= getPageSetting('ScryUseinIce'));
         if (scryInPoisonTrue) {
             return true;
         }
@@ -56,7 +56,6 @@ function useScryerStance() {
     let neverScryInVoid = inVoidMap && (scryInVoidNever && neverScryNormalVoid || neverScryDailyVoid);
     let neverScryInSpire = !onMap && isActiveSpireAT() && scryInSpireNever;
     let neverScryOnBoss = (scryOnBossNeverAboveVoid && currentZoneBelowVMZone && onBossCell) || (scryOnBossNever && onBossCell);
-    let neverScryInNature = (!onMap && !canScryInCurrentNature);
 
     neverScry = neverScry || game.global.world <= 60;
     neverScry = neverScry || neverScryGlobal;
@@ -64,7 +63,7 @@ function useScryerStance() {
     neverScry = neverScry || neverScryInVoid;
     neverScry = neverScry || neverScryInSpire;
     neverScry = neverScry || neverScryOnBoss;
-    neverScry = neverScry || neverScryInNature;
+    neverScry = neverScry || (!game.global.mapsActive && !canScryInCurrentNature);
 
     //check Corrupted Never
     const currentEnemy = getCurrentEnemy(1);
