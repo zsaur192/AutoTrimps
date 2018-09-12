a857febfe295ff75263f5571b9e599aef67d31a8
Zeker0 <zorn@hotmail.co.uk>
2018-09-12 22:58:25 +0100

    Update upgrades.js

--- modules/upgrades.js
+++ modules/upgrades.js
@@ -5,7 +5,7 @@ function buyUpgrades() {
         upgrade = upgradeList[upgrade];
         var gameUpgrade = game.upgrades[upgrade];
         var available = (gameUpgrade.allowed > gameUpgrade.done && canAffordTwoLevel(gameUpgrade));
-        if (upgrade == 'Coordination' && (getPageSetting('BuyUpgradesNew') == 2) || (!canAffordCoordinationTrimps())) continue;
+        if (upgrade == 'Coordination' && (getPageSetting('BuyUpgradesNew') == 2 || !canAffordCoordinationTrimps())) continue;
         if (upgrade == 'Coordination' && (!canAffordCoordinationTrimps() || (getPageSetting('ultwind') >= 1 && game.global.world >= getPageSetting('ultwind') && HDratioy() < getPageSetting('ultwindcut') && game.global.challengeActive != "Daily"))) continue;
         if (upgrade == 'Coordination' && (!canAffordCoordinationTrimps() || (getPageSetting('dultwind') >= 1 && game.global.world >= getPageSetting('dultwind') && HDratioy() < getPageSetting('dultwindcut') && game.global.challengeActive == "Daily"))) continue;
         if (upgrade == 'Coordination' && getPageSetting('amalcoord')==true && ((game.global.world < getPageSetting('amalcoordz') || getPageSetting('amalcoordz') < 0) && (getPageSetting('amalcoordt') < 0 && getPageSetting('amalcoordhd') > 0 && HDratioy() < getPageSetting('amalcoordhd')) || (getPageSetting('amalcoordhd') > 0 && getPageSetting('amalcoordt') > 0 && HDratioy() < getPageSetting('amalcoordhd') && getPageSetting('amalcoordt') > game.jobs.Amalgamator.owned && (game.resources.trimps.realMax() / game.resources.trimps.getCurrentSend()) > 2000))) continue;
