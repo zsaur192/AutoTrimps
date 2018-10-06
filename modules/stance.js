b5e48c099d1d3a81fd407c1bfb41702907e20b26
Zeker0 <zorn@hotmail.co.uk>
2018-10-06 10:26:26 +0100

    Update stance.js

--- modules/stance.js
+++ modules/stance.js
@@ -15,13 +15,13 @@ function autoStanceNew() {
 	
 	if (!game.global.preMapsActive && game.global.soldierHealth > 0) {
 	
-		if (stayD && game.global.formation != 2) {
+		if (stayD) {
 			setFormation(2);
 		}
-		if (stayX && game.global.formation != 0) {
+		else if (stayX) {
 			setFormation(0);
 		}
-		if (stayH && game.global.formation != 1) {
+		else if (stayH) {
 			setFormation(1);
 		}
 	}
