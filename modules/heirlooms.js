44cc7087d54cd084247421e3fe4262e85a9f7b54
Zeker0 <zorn@hotmail.co.uk>
2018-12-06 01:14:24 +0000

    Update heirlooms.js

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -421,16 +421,18 @@ function heirloomSwapping() {
 //daily loom swapping
 function dhighdmgshield(){for(loom of game.global.heirloomsCarried)if(loom.name==getPageSetting('dhighdmg'))return loom}
 function dlowdmgshield(){for(loom of game.global.heirloomsCarried)if(loom.name==getPageSetting('dlowdmg'))return loom}
-
+var dsuckit = false;
 function dheirloomSwapping() {
     if (getEmpowerment() == "Wind" && !game.global.mapsActive && (HDratioy() < getPageSetting('dloomswaphd') || getPageSetting('dloomswaphd') < 0) && game.global.ShieldEquipped.name == getPageSetting('dhighdmg') && game.empowerments.Wind.currentDebuffPower < getPageSetting('dloomswapstack')) {
         dlowdmgshield();
         selectHeirloom(game.global.heirloomsCarried.indexOf(loom), "heirloomsCarried", true);
         equipHeirloom();
+	dsuckit = true;
     }
-    else /*if (game.global.mapsActive || ((HDratioy() >= getPageSetting('dloomswaphd') || game.empowerments.Wind.currentDebuffPower >= getPageSetting('dloomswapstack')) || (getEmpowerment() != "Wind")) && game.global.ShieldEquipped.name == getPageSetting('dlowdmg'))*/ {
+    else if (!dsuckit) /*(game.global.mapsActive || ((HDratioy() >= getPageSetting('dloomswaphd') || game.empowerments.Wind.currentDebuffPower >= getPageSetting('dloomswapstack')) || (getEmpowerment() != "Wind")) && game.global.ShieldEquipped.name == getPageSetting('dlowdmg'))*/ {
         dhighdmgshield();
         selectHeirloom(game.global.heirloomsCarried.indexOf(loom), "heirloomsCarried", true);
         equipHeirloom();
+	dsuckit = false;
     }
 }
