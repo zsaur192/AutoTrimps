8554e78ca8086dc0b868eb4b6d53d3776d2c346c
Zeker0 <zorn@hotmail.co.uk>
2018-12-10 20:27:33 +0000

    Update breedtimer.js

--- modules/breedtimer.js
+++ modules/breedtimer.js
@@ -3,6 +3,9 @@ MODULES["breedtimer"].voidCheckPercent = 95;
 
 function ATGA() {
 	if (game.jobs.Geneticist.locked == false && getPageSetting('ATGA') == true && getPageSetting('ATGAtimer') > 0){
+		var trimps = game.resources.trimps;
+		var trimpsMax = trimps.realMax();
+		var maxBreedable = new DecimalBreed(trimpsMax).minus(trimps.employed);
 		var currentSend = game.resources.trimps.getCurrentSend();
 		var totalTime = DecimalBreed.log10(maxBreedable.div(maxBreedable.minus(currentSend))).div(DecimalBreed.log10(potencyMod)).div(10);
 		var target = getPageSetting('ATGAtimer');
