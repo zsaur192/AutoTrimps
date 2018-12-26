26e8cc0e969d397bf901844b25b727dee4faed25
Zeker0 <zorn@hotmail.co.uk>
2018-12-26 13:42:15 +0000

    Update other.js

--- modules/other.js
+++ modules/other.js
@@ -563,13 +563,11 @@ else if (perked == true && game.global.world !== getPageSetting('lootdumpz')) {
 }
 
 function trimpcide() {
-if (game.portal.Anticipation.level >= 1) {
-	var antistacklimit = 45;
-	if (!game.talents.patience.purchased) 
-	    antistacklimit = 30;
-	if (getPageSetting('fuckanti') > 0 && getEmpowerment() == "Wind" && game.global.world >= getPageSetting('hardcorewind') && (game.global.world < getPageSetting('hardcorewindmax') || getPageSetting('hardcorewindmax') < 1) && HDratioy() < getPageSetting('fuckanti'))
-	    antistacklimit = 0;
-	if (game.global.fighting && ((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && (game.global.antiStacks < antistacklimit || antistacklimit == 0 && game.global.antiStacks >= 1) && !game.global.spireActive)
+if (game.portal.Anticipation.level > 0) {
+	var antistacklimit = (game.talents.patience.purchased) ? 45 : 30;
+	if (game.global.antiStacks > 1 && getPageSetting('fuckanti') > 0 && getEmpowerment() == "Wind" && HDratioy() < getPageSetting('fuckanti') && !game.global.preMapsActive && !game.global.mapsActive)
+              forceAbandonTrimps();
+	else if (game.global.fighting && ((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && (game.global.antiStacks < antistacklimit || antistacklimit == 0 && game.global.antiStacks >= 1) && !game.global.spireActive)
               forceAbandonTrimps();
 	if (game.global.fighting && ((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && game.global.antiStacks < antistacklimit && game.global.mapsActive) {
 	      if (getCurrentMapObject().location == "Void") {
