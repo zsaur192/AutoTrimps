3132cba1ab69fd3c5a1b1e7959cf0da8eba796b6
Saeldur <5496498+Saeldur@users.noreply.github.com>
2019-01-05 16:40:21 +0000

    Allow game to run first to guarantee all variables are always correctly populated

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -56,7 +56,7 @@ function printChangelog() {
 
 var runInterval=100,startupDelay=4000;
 setTimeout(delayStart,startupDelay);
-function gameLoopHook(makeUp, now) {mainLoop(),gameRunGameLoop(makeUp, now)}
+function gameLoopHook(makeUp, now) {gameRunGameLoop(makeUp, now),mainLoop()}
 function delayStart(){initializeAutoTrimps(),printChangelog(),setTimeout(delayStartAgain,startupDelay)}function delayStartAgain(){game.global.addonUser=!0,game.global.autotrimps=!0,MODULESdefault=JSON.parse(JSON.stringify(MODULES)),runGameLoop=gameLoopHook,setInterval(guiLoop,10*runInterval),autoTrimpSettings.PrestigeBackup!==void 0&&''!=autoTrimpSettings.PrestigeBackup.selected&&(document.getElementById('Prestige').value=autoTrimpSettings.PrestigeBackup.selected),''===document.getElementById('Prestige').value&&(document.getElementById('Prestige').value='Off')}
 var ATrunning=!0,ATmessageLogTabVisible=!0,enableDebug=!0,autoTrimpSettings={},MODULES={},MODULESdefault={},ATMODULES={},ATmoduleList=[],bestBuilding,scienceNeeded,breedFire=!1,shouldFarm=!1,enoughDamage=!0,enoughHealth=!0,baseDamage=1,baseBlock=1,baseHealth=1,preBuyAmt,preBuyFiring,preBuyTooltip,preBuymaxSplit,currentworld=0,lastrunworld=0,aWholeNewWorld=!1,needGymystic=!0,heirloomFlag=!1,heirloomCache=game.global.heirloomsExtra.length,magmiteSpenderChanged=!1,daily3=!1;
 
