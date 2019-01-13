e6c6e7538c26abb2b2c974663ebca5ecb89235aa
Zeker0 <zorn@hotmail.co.uk>
2019-01-13 12:30:15 +0000

    Update calc.js

--- modules/calc.js
+++ modules/calc.js
@@ -322,7 +322,11 @@ function calcDailyAttackMod(number) {
 }
 
 function calcSpire(cell, name, what) {
-	var exitCell = isActiveSpireAT() && getPageSetting('ExitSpireCell') > 0 && getPageSetting('ExitSpireCell') <= 100 ? getPageSetting('ExitSpireCell') - 1 : cell;
+	var exitCell = cell;
+	if (game.global.challengeActive != "Daily" && isActiveSpireAT() && getPageSetting('ExitSpireCell') > 0 && getPageSetting('ExitSpireCell') <= 100)
+		exitCell = (getPageSetting('ExitSpireCell') - 1);
+	if (game.global.challengeActive != "Daily" && disActiveSpireAT() && getPageSetting('dExitSpireCell') > 0 && getPageSetting('dExitSpireCell') <= 100)
+		exitCell = (getPageSetting('ExitSpireCell') - 1);
 	var enemy = cell == 99 ? (exitCell == 99 ? game.global.gridArray[99].name : "Snimp") : name;
 	var base = (what == "attack") ? game.global.getEnemyAttack(exitCell, enemy, false) : (game.global.getEnemyHealth(exitCell, enemy, false) * 2);
 	var mod = (what == "attack") ? 1.17 : 1.14;
