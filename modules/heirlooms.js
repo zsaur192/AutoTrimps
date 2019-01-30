249105010ef800530e5552cc59da987b78635b1b
Zeker0 <zorn@hotmail.co.uk>
2019-01-30 15:45:44 +0000

    Update heirlooms.js

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -196,3 +196,78 @@ function dhighHeirloom() {
         equipHeirloom();
 	}
 }
+
+function nuloom(slot) {
+    var loomfound = false;
+    if (game.global.ShieldEquipped.name == getPageSetting('heirloomnu')) {
+        loomfound = true;
+    }
+    if (loomfound) {
+        selectHeirloom(-1, 'ShieldEquipped', true);
+        if (slot == 0) {
+            return game.global.ShieldEquipped.mods[0][0];
+        }
+        if (slot == 1) {
+            return game.global.ShieldEquipped.mods[1][0];
+        }
+        if (slot == 2) {
+            return game.global.ShieldEquipped.mods[2][0];
+        }
+        if (slot == 3) {
+            return game.global.ShieldEquipped.mods[3][0];
+        }
+        if (slot == 4) {
+            return game.global.ShieldEquipped.mods[4][0];
+        }
+    }
+
+    if (!loomfound) {
+        if (game.global.StaffEquipped.name == getPageSetting('heirloomnu')) {
+            loomfound = true;
+        }
+        if (loomfound) {
+            selectHeirloom(-1, 'StaffEquipped', true);
+            if (slot == 0) {
+                return game.global.StaffEquipped.mods[0][0];
+            }
+            if (slot == 1) {
+                return game.global.StaffEquipped.mods[1][0];
+            }
+            if (slot == 2) {
+                return game.global.StaffEquipped.mods[2][0];
+            }
+            if (slot == 3) {
+                return game.global.StaffEquipped.mods[3][0];
+            }
+            if (slot == 4) {
+                return game.global.StaffEquipped.mods[4][0];
+            }
+        }
+    }
+
+    if (!loomfound) {
+        for (var loom of game.global.heirloomsCarried) {
+            if (loom.name == getPageSetting('heirloomnu')) {
+                loomfound = true;
+            }
+            if (loomfound) {
+                selectHeirloom(game.global.heirloomsCarried.indexOf(loom), "heirloomsCarried", true);
+                if (slot == 0) {
+                    return loom.mods[0][0];
+                }
+                if (slot == 1) {
+                    return loom.mods[1][0];
+                }
+                if (slot == 2) {
+                    return loom.mods[2][0];
+                }
+                if (slot == 3) {
+                    return loom.mods[3][0];
+                }
+                if (slot == 4) {
+                    return loom.mods[4][0];
+                }
+            }
+        }
+    }
+}
