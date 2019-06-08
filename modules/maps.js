1a0d4f10940e26b696a2a0b31b99fc752032c725
Zeker0 <zorn@hotmail.co.uk>
2019-06-08 13:23:45 +0100

    Update maps.js

--- modules/maps.js
+++ modules/maps.js
@@ -126,7 +126,7 @@ function autoMap() {
     var prestige = autoTrimpSettings.Prestige.selected;
     if (prestige != "Off" && game.options.menu.mapLoot.enabled != 1) toggleSetting('mapLoot');
     if (game.global.repeatMap == true && !game.global.mapsActive && !game.global.preMapsActive) repeatClicked();
-    if ((game.options.menu.repeatUntil.enabled == 1 || game.options.menu.repeatUntil.enabled == 2) && !game.global.mapsActive && !game.global.preMapsActive) toggleSetting('repeatUntil');
+    if ((game.options.menu.repeatUntil.enabled == 1 || game.options.menu.repeatUntil.enabled == 2 || game.options.menu.repeatUntil.enabled == 3) && !game.global.mapsActive && !game.global.preMapsActive) toggleSetting('repeatUntil');
     if (game.options.menu.exitTo.enabled != 0) toggleSetting('exitTo');
     if (game.options.menu.repeatVoids.enabled != 0) toggleSetting('repeatVoids');
     var challSQ = game.global.runningChallengeSquared;
