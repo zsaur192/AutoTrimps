71f0616cdc817465f9eb6dff27675f35fd05441a
Zeker0 <zorn@hotmail.co.uk>
2019-08-08 23:03:28 +0100

    Update calc.js

--- modules/calc.js
+++ modules/calc.js
@@ -289,6 +289,9 @@ function calcOurDmg(minMaxAvg, incStance, incFlucts) {
 	if (Fluffy.isActive()){
 		number *= Fluffy.getDamageModifier();
 	}
+	if (getHeirloomBonus("Shield", "gammaBurst") > 0 && (calcOurHealth() / (calcBadGuyDmg(null, getEnemyMaxAttack(game.global.world, 50, 'Snimp', 1.0))) >= 5)) {
+	    	number *= ((getHeirloomBonus("Shield", "gammaBurst") / 100) + 1) / 5;
+	}
 
 
   if (!incStance && game.global.formation != 0) {
