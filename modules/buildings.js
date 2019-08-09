721b3464bd04e1a634a1f45fa4a78acd59dc9bf7
Zeker0 <zorn@hotmail.co.uk>
2019-08-09 11:14:46 +0100

    Update buildings.js

--- modules/buildings.js
+++ modules/buildings.js
@@ -306,7 +306,7 @@ function RbuyFoodEfficientHousing() {
     if (game.buildings[bb.name].owned < max || max == -1) {
         bestfoodBuilding = bb.name;
     }
-    if (bestfoodBuilding != undefined && smithylogic(bestfoodBuilding, 'wood', false) && bestfoodBuilding) {
+    if (smithylogic(bestfoodBuilding, 'wood', false) && bestfoodBuilding) {
         document.getElementById(bestfoodBuilding).style.border = "1px solid #00CC01";
         RsafeBuyBuilding(bestfoodBuilding);
     }
@@ -342,7 +342,7 @@ function RbuyGemEfficientHousing() {
         }
         break;
     }
-	if (bestBuilding != undefined && smithylogic(bestBuilding, 'gems', false) && bestBuilding) {
+	if (smithylogic(bestBuilding, 'gems', false) && bestBuilding) {
         RsafeBuyBuilding(bestBuilding);
     }
 }
