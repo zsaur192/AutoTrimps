e9a01018c68c0dcc9448369d71d0e9fb4f2c0188
mkon <mkon@itu.dk>
2019-08-20 19:42:36 +0200

    refactored universes

--- .user.js
+++ .user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
-// @name         AutoTrimps-madsgkk
+// @name         AutoTrimps-Zek
 // @version      1.0-Zek
-// @namespace    https://madsgkk.github.io/AutoTrimps
-// @updateURL    https://madsgkk.github.io/AutoTrimps/.user.js
+// @namespace    https://Zorn192.github.io/AutoTrimps
+// @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *madsgkk.github.io/AutoTrimps*
+// @connect      *Zorn192.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
-// @grant        none
+// @grant        GM_xmlhttpRequest 
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Arche';
+script.id = 'AutoTrimps-Zek';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://madsgkk.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
