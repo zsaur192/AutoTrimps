97aaddb07a51c051daf9249cd52eb47424c45c1b
Zeker0 <zorn@hotmail.co.uk>
2019-08-20 09:08:50 +0100

    Update heirlooms.js

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -434,7 +434,7 @@ function spendNu() {
 
 function generateHeirloomIcon(heirloom, location, number){
     if (typeof heirloom.name === 'undefined') return "<span class='icomoon icon-sad3'></span>";
-    var icon = getHeirloomIcon(heirloom.type);
+    var icon = getHeirloomIcon(heirloom);
     var animated = (game.options.menu.showHeirloomAnimations.enabled) ? "animated " : "";
     var html = '<span class="heirloomThing ' + animated + 'heirloomRare' + heirloom.rarity;
     if (location == "Equipped") html += ' equipped';
