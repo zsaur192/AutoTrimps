c44e9ca400b8ad8630588e4ac73a0179a04423f2
Zeker0 <zorn@hotmail.co.uk>
2019-10-06 22:23:29 +0100

    Update perks.js

--- modules/perks.js
+++ modules/perks.js
@@ -1352,7 +1352,7 @@ RAutoPerks.initializePerks = function () {
     var bait = new RAutoPerks.FixedPerk("bait", 4, 30);
     var trumps = new RAutoPerks.FixedPerk("trumps", 3, 30);
     var packrat = new RAutoPerks.FixedPerk("packrat", 3, 30);
-    var overkill = new AutoPerks.FixedPerk("overkill", 1000000, true,      10, 0.005, 30);
+    var overkill = new RAutoPerks.FixedPerk("overkill", 1000000, true,      10, 0.005, 30);
     //variable
     var looting = new RAutoPerks.VariablePerk("looting", 1, false,             0, 0.05);
     var toughness = new RAutoPerks.VariablePerk("toughness", 1, false,         1, 0.05);
@@ -1362,9 +1362,9 @@ RAutoPerks.initializePerks = function () {
     var artisanistry = new RAutoPerks.VariablePerk("artisanistry", 15, true,   5, 0.1);
     var carpentry = new RAutoPerks.VariablePerk("carpentry", 25, true,         6, 0.1);
     var prismal = new RAutoPerks.VariablePerk("prismal", 1, true,              7, 0.1);
-    var resilience = new AutoPerks.VariablePerk("resilience", 100, true,       9, 0.1);
-    var criticality = new AutoPerks.VariablePerk("criticality", 100, true,     10, 0.1);
-    var tenacity = new AutoPerks.VariablePerk("tenacity", 50000000, true,      11, 0.1);
+    var resilience = new RAutoPerks.VariablePerk("resilience", 100, true,       9, 0.1);
+    var criticality = new RAutoPerks.VariablePerk("criticality", 100, true,     10, 0.1);
+    var tenacity = new RAutoPerks.VariablePerk("tenacity", 50000000, true,      11, 0.1);
     // Equality is a 9/10 multiplier on enemy damage, which is like a 10/9 multiplier on your health.  So we pretend the bonus for this perk is 1/9.
     var equality = new RAutoPerks.VariablePerk("equality", 1, true,            12, 0.11111);
     equality.exprate = 1.5;
