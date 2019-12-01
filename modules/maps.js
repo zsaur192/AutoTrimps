ee73a7702fffac15ba335c535e17ec7dd80a6fd9
Zeker0 <zorn@hotmail.co.uk>
2019-12-01 15:26:26 +0000

    Update maps.js

--- modules/maps.js
+++ modules/maps.js
@@ -1263,7 +1263,7 @@ function RautoMap() {
 	            var levelzones = timefarmlevel[timefarmlevelindex];
 			
 		    if (timefarmzone.includes(game.global.world)) {
-			if (levelzones >= 0) {
+			if (levelzones > 0) {
 			    $mapLevelInput.value = game.global.world;
 			    document.getElementById("advExtraLevelSelect").value = levelzones;
 			}
