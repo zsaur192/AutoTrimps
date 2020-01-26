a29bbe4044419368420eafe5004bce029d44ce0c
Zeker0 <zorn@hotmail.co.uk>
2020-01-26 13:22:17 +0000

    Update maps.js

--- modules/maps.js
+++ modules/maps.js
@@ -989,7 +989,7 @@ function RautoMap() {
             Rshouldtimefarm = true;
 	}
 
-        if (game.global.challengeActive == "Quagmire" && getPageSetting('Rtimefarmbog') == true && timefarmzone.includes(70) && game.global.world == 70 && game.global.mapsOwnedArray[getMapIndex(game.global.currentMapId)].name == "The Black Bog" && (Rshouldtimefarmbogs && game.global.lastClearedMapCell >= 145 || timezones <= time)) {
+        if (game.global.challengeActive == "Quagmire" && getPageSetting('Rtimefarmbog') == true && timefarmzone.includes(70) && game.global.world == 70 && game.global.mapsActive && game.global.mapsOwnedArray[getMapIndex(game.global.currentMapId)].name == "The Black Bog" && (Rshouldtimefarmbogs && game.global.lastClearedMapCell >= 145 || timezones <= time)) {
 	    mapsClicked(true);
 	}
     }
