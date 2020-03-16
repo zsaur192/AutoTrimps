26c230c336269f1581a7d6168cdfa8116e8e4ece
Zeker0 <zorn@hotmail.co.uk>
2020-03-16 23:55:45 +0000

    Update query.js

--- modules/query.js
+++ modules/query.js
@@ -105,3 +105,13 @@ function getPotencyMod(howManyMoreGenes) {
     potencyMod = calcHeirloomBonus("Shield", "breedSpeed", potencyMod);
     return potencyMod;
 }
+
+function getArmyTime() {
+    var breeding = (game.resources.trimps.owned - game.resources.trimps.employed);
+    var newSquadRdy = game.resources.trimps.realMax() <= game.resources.trimps.owned + 1;
+    var adjustedMax = (game.portal.Coordinated.level) ? game.portal.Coordinated.currentSend : game.resources.trimps.maxSoldiers;
+    var potencyMod = getPotencyMod();
+    var tps = breeding * potencyMod;
+    var addTime = adjustedMax / tps;
+    return addTime;
+}
