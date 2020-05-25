5da1bbb7ab0b3e3ba682d208ac6bddb45654117a
Zeker0 <zorn@hotmail.co.uk>
2020-05-25 15:56:32 +0100

    Update fight-info.js

--- modules/fight-info.js
+++ modules/fight-info.js
@@ -1,1 +1,192 @@
-(function(a){a.fightinfo={},a.fightinfo.$worldGrid=document.getElementById("grid"),a.fightinfo.$mapGrid=document.getElementById("mapGrid"),a.fightinfo.powerful=["Improbability","Omnipotrimp","Mutimp","Hulking_Mutimp"],a.fightinfo.exotics=["Feyimp","Tauntimp","Venimp","Whipimp","Magnimp","Goblimp","Flutimp","Jestimp","Titimp","Chronoimp"],a.fightinfo.colors={bone:"#ffffff",exotic:"#000000",powerful:"#000000"},a.fightinfo.lastProcessedWorld=null,a.fightinfo.lastProcessedMap=null,a.fightinfo.Update=function(){if(game.global.mapsActive)var c=game.global.mapGridArray,d=Array.prototype.slice.call(a.fightinfo.$mapGrid.children);else{if(null===a.fightinfo.lastProcessedWorld||a.fightinfo.lastProcessedWorld!==game.global.world)a.fightinfo.lastProcessedWorld=game.global.world;else return;var c=game.global.gridArray,d=Array.prototype.slice.call(a.fightinfo.$worldGrid.children)}d=d.reverse();var e=[];d.forEach(function(j){e=e.concat(Array.prototype.slice.call(j.children))});for(var f=0;f<e.length;f++){var g=e[f],h=c[f];-1<h.name.toLowerCase().indexOf("skele")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML+=`<span class=\"glyphicon glyphicon-italic\"></span>`,g.style.textShadow="0px 0px 15px #ffffff"):-1<a.fightinfo.powerful.indexOf(h.name)?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML+="<span class=\"glyphicon glyphicon-hazard\"></span> ",g.style.textShadow="0px 0px 15px #8c0000"):-1<h.name.toLowerCase().indexOf("cthulimp")?g.style.textShadow="0px 0px 15px #fb753f":-1<h.name.toLowerCase().indexOf("goblimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML="<span class=\"icomoon icon-evil\"></span> ",g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("flutimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML="<span class=\"glyphicon glyphicon-globe\"></span> ",g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("jestimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML="<span class=\"icomoon icon-mask\"></span> ",g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("titimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML="<span class=\"icomoon icon-hammer\"></span> ",g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("chronoimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML="<span class=\"glyphicon glyphicon-hourglass\"></span> ",g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("feyimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML+=`<span class=\"icomoon icon-diamond\"></span>`,g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("tauntimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML+=`<span class=\"glyphicon glyphicon-tent\"></span>`,g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("venimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML+=`<span class=\"glyphicon glyphicon-baby-formula\"></span>`,g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("whipimp")?(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML+=`<span class=\"icomoon icon-area-graph\"></span>`,g.style.textShadow="0px 0px 15px #fb753f"):-1<h.name.toLowerCase().indexOf("magnimp")&&(0!==h.special.length&&(g.innerHTML+=" "),g.innerHTML+=`<span class=\"glyphicon glyphicon-magnet\"></span>`,g.style.textShadow="0px 0px 15px #fb753f"),null!=h.corrupted&&"none"!==h.corrupted?g.title+=`${h.name} \- ${mutationEffects[h.corrupted].title}`:game.global.mapsActive?g.title=`${h.name}`:g.title+=` ${h.name}`}}})(MODULES);
+;(function(M)
+{
+	M["fightinfo"] = {};
+	M["fightinfo"].$worldGrid = document.getElementById('grid');
+	M["fightinfo"].$mapGrid = document.getElementById('mapGrid');
+
+	// Powerful imps
+	M["fightinfo"].powerful =
+	[
+		"Improbability",
+		"Omnipotrimp",
+		"Mutimp",
+		"Hulking_Mutimp"
+	];
+
+	// Exotic imps
+	M["fightinfo"].exotics =
+	[
+		"Feyimp",
+		"Tauntimp",
+		"Venimp",
+		"Whipimp",
+		"Magnimp",
+		"Goblimp",
+		"Flutimp",
+		"Jestimp",
+		"Titimp",
+		"Chronoimp"
+	];
+  
+  // Fast imps
+	M["fightinfo"].fast =
+	[
+		"Snimp",
+    "Kittimp",
+    "Gorillimp",
+    "Squimp",
+    "Shrimp",
+    "Chickimp",
+    "Frimp",
+    "Slagimp",
+    "Lavimp",
+    "Kangarimp",
+    "Entimp",
+    "Fusimp",
+    "Carbimp",
+    "Shadimp",
+    "Voidsnimp",
+    "Prismimp",
+    "Sweltimp"
+	];
+
+	//Colors for special imps (This has been disabled)
+	M["fightinfo"].colors =
+	{
+		bone: '#ffffff',
+		exotic: '#000000',
+		powerful: '#000000',
+    fast : '#000000'
+	}
+
+	M["fightinfo"].lastProcessedWorld = null;
+	M["fightinfo"].lastProcessedMap = null;
+
+	function Update()
+	{
+		// Check if we should update world or map info
+		if(game.global.mapsActive)
+		{
+			// Check if current map already infoed
+			// Can't do this because of map repeating
+			/*if(M["fightinfo"].lastProcessedMap === null || M["fightinfo"].lastProcessedMap !== game.global.lookingAtMap)
+				M["fightinfo"].lastProcessedMap = game.global.lookingAtMap;
+			else
+				return;*/
+
+			// Cell data
+			var cells = game.global.mapGridArray;
+
+			// DOM rows
+			var $rows = Array.prototype.slice.call(M["fightinfo"].$mapGrid.children);
+		}
+		else
+		{
+			// Check if current world already infoed
+			if(M["fightinfo"].lastProcessedWorld === null || M["fightinfo"].lastProcessedWorld !== game.global.world)
+				M["fightinfo"].lastProcessedWorld = game.global.world;
+			else
+				return;
+
+			// Cell data
+			var cells = game.global.gridArray;
+
+			// DOM rows
+			var $rows = Array.prototype.slice.call(M["fightinfo"].$worldGrid.children);
+		}
+
+		// Rows are in inverse order somewhy
+		$rows = $rows.reverse();
+
+		// DOM cells
+		var $cells = [];
+
+		// Loop through DOM rows and concat each row's cell-element into $cells
+		$rows.forEach(function(x)
+		{
+			$cells = $cells.concat(Array.prototype.slice.call(x.children));
+		});
+
+		// Process all cells
+		for(var i = 0; i < $cells.length; i++)
+		{
+			// DOM cell
+			var $cell = $cells[i];
+
+			// Cell data
+			var cell = cells[i];
+
+
+			if(cell.name.toLowerCase().indexOf('skele') > -1)					// Skeletimp cell
+			{
+				if(cell.special.length === 0)
+					$cell.innerHTML = "<span class=\"glyphicon glyphicon-italic\"></span> ";
+
+				$cell.title = cell.name;
+				//$cell.style.color = M["fightinfo"].colors.bone; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
+				$cell.style.textShadow = '0px 0px 10px #ffffff';
+			}
+
+			else if(M["fightinfo"].exotics.indexOf(cell.name) > -1)				// Exotic cell
+			{
+				if(cell.special.length === 0)
+					$cell.innerHTML = "<span class=\"glyphicon glyphicon-sunglasses\"></span> ";
+
+				$cell.title = cell.name;
+				//$cell.style.color = M["fightinfo"].colors.exotic; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
+				$cell.style.textShadow = '0px 0px 10px #fb753f';
+			}
+
+			else if(M["fightinfo"].powerful.indexOf(cell.name) > -1)			// Powerful imp
+			{
+				if(cell.special.length === 0)
+					$cell.innerHTML = "<span class=\"glyphicon glyphicon-hazard\"></span> ";
+
+				$cell.title = cell.name;
+				//$cell.style.color = M["fightinfo"].colors.powerful; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
+				$cell.style.textShadow = '0px 0px 10px #8c0000';
+			}
+      
+      else if(M["fightinfo"].fast.indexOf(cell.name) > -1)			// Fast imp
+			{
+				if(cell.special.length === 0)
+					$cell.innerHTML = "<span class=\"glyphicon glyphicon-forward\"></span> ";
+
+				$cell.title = cell.name;
+				//$cell.style.color = M["fightinfo"].colors.fast; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
+				$cell.style.textShadow = '0px 0px 10px #8c0000';
+			}
+
+			//This shit doesn't work and I don't know why (What is the celltitle??? is it the name of the nature? Imps are labelled Toxic/Gusty/Frozen but that didin't work either)
+			if(cell.name.toLowerCase().indexOf('poison') > -1)				// Poison Token cell
+			{
+			  if(cell.special.length === 0)
+			    $cell.innerHTML = "<span class=\"glyphicon glyphicon-flask\"></span> ";
+
+			  $cell.title = cell.name;
+			  //$cell.style.color = M["fightinfo"].colors.exotic; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
+			  $cell.style.textShadow = '0px 0px 10px #ffffff';
+			}
+			if(cell.name.toLowerCase().indexOf('wind') > -1)				// Wind Token cell
+			{
+			  if(cell.special.length === 0)
+			    $cell.innerHTML = "<span class=\"icomoon icon-air\"></span> ";
+
+			  $cell.title = cell.name;
+			  //$cell.style.color = M["fightinfo"].colors.exotic; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
+			  $cell.style.textShadow = '0px 0px 10px #ffffff';
+			}
+			if(cell.name.toLowerCase().indexOf('ice') > -1)				// Ice Token cell
+			{
+			  if(cell.special.length === 0)
+			    $cell.innerHTML = "<span class=\"glyphicon glyphicon-certificate\"></span> ";
+
+			  $cell.title = cell.name;
+			  //$cell.style.color = M["fightinfo"].colors.exotic; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
+			  $cell.style.textShadow = '0px 0px 10px #ffffff';
+			}
+		}
+	}
+
+	M["fightinfo"].Update = Update;
+})(MODULES);
